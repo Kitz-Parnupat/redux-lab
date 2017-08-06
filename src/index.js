2d0179522f2b6c61f1fb70cba6e5c4ba281cb46c
dainokung2535@gmail.com <dainokung2535@gmail.com>
2017-08-06 15:18:11 +0630

    basic state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+import {createStore} from "redux"
+const reducer = (state,action)=>{
+    switch (action.type) {
+        case "ADD":
+                state+=action.payload;
+            break;
+        case "SUBTRACT":
+                state-=action.payload;
+            break;
+    
+        default:
+            break;
+    }
+    return state;
+}
+const store=createStore(reducer,15000);
+
+store.subscribe(()=>{
+    console.log("update store:",store.getState())
+})
+
+store.dispatch({
+    type:"ADD",
+    payload:500
+});
+store.dispatch({
+    type:"SUBTRACT",
+    payload:10000
+});
